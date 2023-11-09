83ac951dd9b7fba4e2992668bfd8336ec3d0cc37
KalmeMarq <145478572+fmarquescc@users.noreply.github.com>
2023-11-09 15:34:31 +0000

    terrain.png

--- src/main/java/me/kalmemarq/TextureManager.java
+++ src/main/java/me/kalmemarq/TextureManager.java
@@ -1,13 +1,21 @@
 package me.kalmemarq;
 
+import org.lwjgl.opengl.GL11;
+
 public class TextureManager {
-    public class Texture {
+    public static class Texture {
+        private int id;
+        private int width;
+        private int height;
+        
+        public void bind() {
+            GL11.glBindTexture(GL11.GL_TEXTURE_2D, this.id);
+        }
+        
         public void load() {
         }
     }
 
-    public class AtlasTexture extends Texture {
-        public void load() {
-        }
+    public static class AtlasTexture extends Texture {
     }
 }
